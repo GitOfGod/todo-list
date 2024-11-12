c95254977cb6600e2bbf2e2a31e97abddf7c2a18
Favour <favouramike@gmail.com>
2024-11-12 22:11:34 +0000

    Create TodoItem class and import into index.js
    - Add webpack configuration to open browser automatically

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import TodoItem from "./TodoItem";
+
+const todo = new TodoItem('task 1', 'a type of task', '02/11/2001', 1);
+console.log(todo.getDetails());
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,11 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    open: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
